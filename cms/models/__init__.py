0646a1f47fbaa054c690d984037c9579c6450776
Guillaume Libersat <glibersat@sigill.org>
2012-07-02 11:22:01 +0200

    remove monkey patching of django 'reverse'

--- cms/models/__init__.py
+++ cms/models/__init__.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings as d_settings
 from django.core.exceptions import ImproperlyConfigured
-from django.core.urlresolvers import get_resolver, get_script_prefix, \
-    NoReverseMatch
-from django.utils.encoding import iri_to_uri
 from moderatormodels import *
 from pagemodel import *
 from permissionmodels import *
 from placeholdermodel import *
 from pluginmodel import *
 from titlemodels import *
-import django.core.urlresolvers
 # must be last
 from cms import signals as s_import
-
 
 def validate_settings():
     if not "django.core.context_processors.request" in d_settings.TEMPLATE_CONTEXT_PROCESSORS:
@@ -36,38 +31,5 @@ def remove_current_root(url):
         url = url[len(current_root) - 1:]
     return url
 
-def monkeypatch_reverse():
-    if hasattr(django.core.urlresolvers.reverse, 'cms_monkeypatched'):
-        return
-    django.core.urlresolvers.old_reverse = django.core.urlresolvers.reverse
-    
-    def new_reverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None, current_app=None):
-        url = ''
-        i18n = 'cms.middleware.multilingual.MultilingualURLMiddleware' in settings.MIDDLEWARE_CLASSES
-        lang = None
-        if isinstance(viewname, basestring) and viewname.split(":")[0] in dict(settings.LANGUAGES).keys():
-            lang = viewname.split(":")[0]
-        try:    
-            url = django.core.urlresolvers.old_reverse(viewname, urlconf=urlconf, args=args, kwargs=kwargs, prefix=prefix, current_app=current_app)
-            if lang:
-                url = "/%s%s" % (lang, url)
-        except NoReverseMatch, e:
-            if i18n:            
-                if not lang:
-                    try:    
-                        lang = get_language()
-                        ml_viewname = "%s:%s" % ( lang, viewname)
-                        url = django.core.urlresolvers.old_reverse(ml_viewname, urlconf=urlconf, args=args, kwargs=kwargs, prefix=prefix, current_app=current_app)
-                        return url
-                    except NoReverseMatch:
-                        pass
-            raise e
-        url = remove_current_root(url)
-        return url
-
-    new_reverse.cms_monkeypatched = True
-    django.core.urlresolvers.reverse = new_reverse
-        
 validate_dependencies()
 validate_settings()
-monkeypatch_reverse()
